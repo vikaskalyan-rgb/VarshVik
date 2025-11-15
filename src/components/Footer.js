01ca8ead2064249437cad16eca5b170ae1e277a9
vikaskalyan-rgb <vikaskalyan1102@gmail.com>
2025-11-16 03:32:35 +0530

    dfwgwg

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -48,9 +48,24 @@ function Footer () {
               With love and gratitude<br />
               <span className='year'>© 2026 Varsha & Vikas</span>
             </p>
+
+            {/* ⭐ Built with Love Section */}
+            <div className="flex items-center justify-center lg:py-2 mt-3">
+              Built with
+              <span className="text-primary dark:text-primaryDark text-2xl px-1">
+                &#9825;
+              </span>
+              by&nbsp;
+             
+                Vikas Kalyan
+             
+            </div>
+            {/* End Section */}
+
           </div>
         </div>
       </div>
+
       <div className='footer-wave'>
         <svg viewBox="0 0 1200 120" preserveAspectRatio="none">
           <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25"></path>
